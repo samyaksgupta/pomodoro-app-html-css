47065ebfe0c8ae36f75a8a1a7519646c7cf0d30a
samyaksgupta <127541694+samyaksgupta@users.noreply.github.com>
2023-07-31 07:45:08 +0530

    Update

--- achievo/main.js
+++ achievo/main.js
@@ -104,3 +104,49 @@ ListContainer.addEventListener("click", function(e){
     }
 
 }, false);
+
+$(document).ready(function () {
+    $(".radio-btn").click(function () {
+      $(".radio-inner").toggleClass("active");
+      $("body").toggleClass("dark");
+      $("")
+
+      
+
+    });
+  });
+
+// var icon = document.getElementById("icon");
+
+// icon.onclick = function(){
+//     document.body.classList.toggle
+
+// }
+
+
+
+
+  const linkColor = document.querySelectorAll('.nav__link')
+
+  function colorLink(){
+      linkColor.forEach(l => l.classList.remove('active-link'))
+      this.classList.add('active-link')
+  }
+  
+  linkColor.forEach(l => l.addEventListener('click', colorLink))
+  
+  /*=============== SHOW HIDDEN MENU ===============*/
+  const showMenu = (toggleId, navbarId) =>{
+      const toggle = document.getElementById(toggleId),
+      navbar = document.getElementById(navbarId)
+  
+      if(toggle && navbar){
+          toggle.addEventListener('click', ()=>{
+              /* Show menu */
+              navbar.classList.toggle('show-menu')
+              /* Rotate toggle icon */
+              toggle.classList.toggle('rotate-icon')
+          })
+      }
+  }
+  showMenu('nav-toggle','nav')
